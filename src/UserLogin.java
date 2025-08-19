b19a1fc4695b86c9b36cf29036168db317bc86e4
AyxanHesenzade <ayxanhesen1234@gmail.com>
2025-08-19 15:40:29 +0400

    polindrom

--- src/UserLogin.java
+++ src/UserLogin.java
@@ -1,42 +1,30 @@
 import java.util.Scanner;
 public class UserLogin {
-    public static void main(String[] args) {
-        String userName, password, newPassword, newUserName, messages;
-        userName = "ayxan";
-        password = "ayxan";
 
+    static  boolean isP(int number){
+        int temp = number;
+        int lastNumber;
+        int rNumber = 0;
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("user name: ");
-        newUserName = input.next();
-        System.out.print("password: ");
-        newPassword = input.next();
-
-        if (!(userName.equals(newUserName))){
-            System.out.println("wrong userName");
-        } else if ( !(password.equals(newPassword))) {
-            System.out.println("wrong password");
-            System.out.print("do you change password: ");
-            messages = input.next();
-            if (messages.equals("yes")){
-                System.out.print("please enter  new password: ");
-                newPassword = input.next();
-                password = newPassword;
-                System.out.print("new pasword"+ " " + password );
-            } else {
-                return;
-            }
-
-        } else{
-            System.out.println("welcome");
+        while (temp!=0){
+            lastNumber= temp%10;
+            rNumber = (rNumber*10) + lastNumber;
+            temp = temp/10;
         }
 
+        if (rNumber == number){
+            return  true;
+        } else {
+            return  false;
+        }
 
+    };
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-
-
-
+        int a = input.nextInt();
+        System.out.println(isP(a));
 
     }
 }
